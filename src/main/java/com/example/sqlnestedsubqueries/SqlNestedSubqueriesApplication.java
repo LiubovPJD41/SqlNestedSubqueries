c5b58719578a42542a6ee331c1b5f5acb1019ef4
Liubov P. JD-41 <lovely8419@mail.ru>
2024-09-17 19:47:54 +0100

    Initial commit

---
+++ src/main/java/com/example/sqlnestedsubqueries/SqlNestedSubqueriesApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqlnestedsubqueries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlNestedSubqueriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlNestedSubqueriesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sqlnestedsubqueries/SqlNestedSubqueriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqlnestedsubqueries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlNestedSubqueriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
